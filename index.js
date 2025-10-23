53ae337018d4bab0300d73944da1b44029bbc305
shohelrana2002 <rana2021tang@gmail.com>
2025-10-24 02:05:40 +0600

    delete poast and verifiy token no dupicate adata add

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ const client = new MongoClient(uri, {
   },
 });
 
+const ipTvCollection = client.db("TvCollection").collection("ipTvs");
+const usersCollection = client.db("TvCollection").collection("users");
+const watchTimeCollection = client.db("TvCollection").collection("watchTime");
+
 // Middleware: verify JWT
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
@@ -31,17 +35,46 @@ const verifyToken = (req, res, next) => {
   const token = authHeader.split(" ")[1];
   jwt.verify(token, process.env.secret_key, (err, decoded) => {
     if (err) return res.status(403).json({ message: "Invalid token" });
+
+    if (!decoded || !decoded.email) {
+      return res.status(401).json({ message: "Invalid token data" });
+    }
     req.user = decoded;
     next();
   });
 };
 
+// verify admin
+const verifyAdmin = async (req, res, next) => {
+  if (!req.user || !req.user.email) {
+    return res.status(401).json({ message: "Unauthorized - No user info" });
+  }
+  try {
+    const email = req.user.email;
+    const user = await usersCollection.findOne({ email });
+    if (!user) return res.status(404).json({ message: "User not found" });
+    if (user.role !== "admin")
+      return res.status(403).json({ message: "Forbidden - Admins only" });
+    next();
+  } catch (err) {
+    console.error("verifyAdmin error:", err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
 async function run() {
   try {
     await client.connect();
-    const ipTvCollection = client.db("TvCollection").collection("ipTvs");
-    const usersCollection = client.db("TvCollection").collection("users");
+    // Generate JWT token
+    app.post("/jwt", async (req, res) => {
+      const { email } = req.body;
+      if (!email) return res.status(400).json({ message: "Email required" });
 
+      const token = jwt.sign({ email }, process.env.secret_key, {
+        expiresIn: "7d",
+      });
+      res.json({ token });
+    });
     // GET all channels
     app.get("/", async (req, res) => {
       const result = await ipTvCollection.find().toArray();
@@ -49,14 +82,14 @@ async function run() {
     });
 
     // POST a new channel
-    app.post("/", async (req, res) => {
+    app.post("/", verifyToken, verifyAdmin, async (req, res) => {
       const data = req.body;
       const result = await ipTvCollection.insertOne(data);
       res.json(result);
     });
 
     // DELETE a channel (protected)
-    app.delete("/:id", verifyToken, async (req, res) => {
+    app.delete("/:id", verifyToken, verifyAdmin, async (req, res) => {
       const { id } = req.params;
       if (!ObjectId.isValid(id))
         return res.status(400).json({ success: false, message: "Invalid ID" });
@@ -64,9 +97,29 @@ async function run() {
       const result = await ipTvCollection.deleteOne({ _id: new ObjectId(id) });
       res.json({ success: true, deleteCount: result.deletedCount });
     });
+    //  delete user
+    app.delete("/dashBoard/allUsers/:id", verifyToken, async (req, res) => {
+      const { id } = req.params;
 
+      const userToDelete = await usersCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      if (!userToDelete)
+        return res.status(404).json({ message: "User not found" });
+
+      if (req.user.email === userToDelete.email) {
+        return res
+          .status(403)
+          .json({ message: "You can't delete your own account" });
+      }
+
+      const result = await usersCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.json({ message: "User deleted successfully" });
+    });
     // UPDATE a channel
-    app.put("/:id", verifyToken, async (req, res) => {
+    app.put("/:id", verifyToken, verifyAdmin, async (req, res) => {
       const { id } = req.params;
       const { name, group, logo, url } = req.body;
 
@@ -82,20 +135,41 @@ async function run() {
         return res
           .status(404)
           .json({ success: false, message: "Channel not found" });
-
       res.json({ success: true, message: "Channel updated" });
     });
 
-    // Generate JWT token
-    app.post("/jwt", async (req, res) => {
-      const { email } = req.body;
-      if (!email) return res.status(400).json({ message: "Email required" });
+    // test start
+    app.post("/watch", async (req, res) => {
+      const { channelUrl, channelName, seconds } = req.body;
+      const filter = { channelUrl };
+      const update = {
+        $inc: { totalSeconds: seconds },
+        $set: { channelName, updatedAt: new Date() },
+      };
+      const options = { upsert: true };
 
-      const token = jwt.sign({ email }, process.env.secret_key, {
-        expiresIn: "7d",
-      });
-      res.json({ token });
+      try {
+        const result = await watchTimeCollection.updateOne(
+          filter,
+          update,
+          options
+        );
+        res.json(result);
+      } catch (err) {
+        res.status(500).json({ error: "Something went wrong" });
+      }
     });
+
+    app.get(
+      "/dashboard/watchTime",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const result = await watchTimeCollection.find().toArray();
+        res.send(result);
+      }
+    );
+    // test end
 
     // Add a user
     app.post("/users", async (req, res) => {
@@ -116,10 +190,43 @@ async function run() {
       res.json({ message: "User saved", user: result });
     });
     // user get
-    app.get("/users", async (req, res) => {
+    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
+    // admin set
+    app.patch(
+      "/dashBoard/allUsers/:email",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const email = req.params.email;
+        const { role } = req.body;
+        if (req.user.email === email) {
+          return res.status(403).json({
+            success: false,
+            message: "You cannot change your own role",
+          });
+        }
+        if (!["admin", "user"].includes(role)) {
+          return res.status(400).json({ message: "Invalid role type" });
+        }
+        const result = await usersCollection.updateOne(
+          { email },
+          {
+            $set: { role },
+          }
+        );
+        if (result.modifiedCount > 0) {
+          res.json({ success: true, message: `User role updated to ${role}` });
+        } else {
+          res.json({
+            success: false,
+            message: "No user found or already same role",
+          });
+        }
+      }
+    );
     // Ping MongoDB to verify connection
     await client.db("admin").command({ ping: 1 });
     console.log("Connected to MongoDB successfully!");
